8b8df96964983383666448f06101b0a6597b1d9e
Hordunlarmy <Hordunlarmy@gmail.com>
2024-06-13 15:24:48 +0100

    Refactor error handling in views and custom renderer

--- sigma/users/views.py
+++ sigma/users/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 
 from .models import User
@@ -16,25 +16,20 @@ class UserListView(generics.ListAPIView):
     permission_classes = [permissions.AllowAny]
 
     def list(self, request, *args, **kwargs):
-        error_response = Response(
-            {
-                "message": "Forbidden: super-admin Only",
-                "error": "Forbidden",
-                "statusCode": 403,
-            },
-            status=403,
-        )
 
         # if not request.user.is_authenticated:
         #    return error_response
 
         roles = getattr(request.user, "roles", [])
         if "super-admin" not in roles:
-            return error_response
-
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
-        return Response(serializer.data)
+            return Response(
+                {
+                    "message": "Forbidden: super-admin Only",
+                    "error": "Forbidden",
+                    "statusCode": 403,
+                },
+                status=403,
+            )
 
 
 class UserProfileView(generics.RetrieveAPIView):
@@ -51,14 +46,15 @@ class UserRetrieveDestroyView(generics.RetrieveDestroyAPIView):
     A view for viewing and deleting user accounts.
     """
 
-    queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.AllowAny]
 
-    def retrieve(self, request, *args, **kwargs):
-        try:
-            instance = self.get_object()
-        except Http404:
+    def get_object(self):
+        user_id = self.kwargs["pk"]
+        return User.objects.filter(id=user_id).first()
+
+    def get(self, request, *args, **kwargs):
+        if self.get_object() == None:
             return Response(
                 {
                     "message": "User with this id does not exist",
@@ -67,8 +63,6 @@ class UserRetrieveDestroyView(generics.RetrieveDestroyAPIView):
                 },
                 status=404,
             )
-        serializer = self.get_serializer(instance)
-        return Response(serializer.data)
 
 
 class UserCreateView(generics.CreateAPIView):
