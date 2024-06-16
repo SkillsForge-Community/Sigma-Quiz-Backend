15bd37ca2ac6179a4679a5954928a95d5eba79f5
Hordunlarmy <Hordunlarmy@gmail.com>
2024-06-16 13:19:57 +0100

    Add noqa for unused import in admin.py and remove tests.py

--- sigma/school/views.py
+++ sigma/school/views.py
@@ -1,6 +1,5 @@
 from rest_framework import generics, permissions
 from rest_framework.response import Response
-from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from .models import School
 from .serializers import SchoolSerializer
