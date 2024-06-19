e1b7428b29c568409dc4b7790ccd64ffa92a428a
Hordunlarmy <Hordunlarmy@gmail.com>
2024-06-19 14:36:59 +0100

    Add Round model and serializer with create and retrieve views

---
+++ sigma/round/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+from sigma.quiz.models import Quiz
+from sigma.utils.base_class import BaseModel
+
+
+class Round(BaseModel):
+
+    # school = models.ForeignKey(School, on_delete=models.CASCADE)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name="rounds")
+    name = models.CharField(max_length=100)
+    round_number = models.PositiveIntegerField()
+    no_of_questions = models.PositiveIntegerField()
+    no_of_schools = models.PositiveIntegerField()
+    marks_per_question = models.PositiveIntegerField()
+    marks_per_bonus_question = models.PositiveIntegerField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["quiz", "round_number"],
+                name="unique_school_quiz_round",
+            )
+        ]
+
+    @property
+    def quizId(self):
+        return self.quiz.id
+
+    def __str__(self):
+        return f"Name: {self.name} | Round No: {self.round_number}"
+
+
+class Question(BaseModel):
+    round = models.ForeignKey(Round, on_delete=models.CASCADE, related_name="questions")
+    question_number = models.PositiveIntegerField()
+    correct_answer = models.CharField(max_length=255, null=True, blank=True)
+    answered_by = models.CharField(max_length=100, null=True, blank=True)
+    bonus_to = models.CharField(max_length=100, null=True, blank=True)
+
+    @property
+    def roundId(self):
+        return self.round.id
+
+    def __str__(self):
+        return f"Question: {self.question_number}, Round: {self.round.round_number} ({self.round.name})"
---
+++ sigma/round/serializers.py
@@ -0,0 +1,84 @@
+from rest_framework import serializers
+
+from sigma.quiz.models import Quiz
+
+from .models import Round
+
+
+class QuizRoundSerializer(serializers.ModelSerializer):
+    quiz = serializers.SerializerMethodField()
+    quizId = serializers.UUIDField()
+
+    class Meta:
+        model = Round
+        fields = [
+            "quizId",
+            "name",
+            "round_number",
+            "no_of_questions",
+            "no_of_schools",
+            "marks_per_question",
+            "marks_per_bonus_question",
+            "quiz",
+            "id",
+            "created_at",
+            "updated_at",
+            "questions",
+        ]
+
+    def get_quiz(self, instance):
+        quiz_obj = instance.quiz
+        return {
+            "id": quiz_obj.id,
+            "year": quiz_obj.year,
+            "title": quiz_obj.title,
+            "description": quiz_obj.description,
+            "date": quiz_obj.date,
+        }
+
+    def validate(self, data):
+
+        quiz_id = data.get("quizId")
+        round_number = data.get("round_number")
+
+        try:
+            quiz = Quiz.objects.get(id=quiz_id)
+        except Quiz.DoesNotExist:
+            raise serializers.ValidationError(
+                {
+                    "message": "Sigma Quiz with this id does not exis",
+                    "error": "Not Found",
+                    "statusCode": 404,
+                }
+            )
+        if Round.objects.filter(quiz=quiz, round_number=round_number).exists():
+            raise serializers.ValidationError(
+                {
+                    "message": f'Key ("quizId", round_number)=({quiz_id}, {round_number}) already exists.',
+                    "error": "Conflict",
+                    "statusCode": 409,
+                }
+            )
+
+        return data
+
+    def to_representation(self, instance):
+        request = self.context.get("request")
+        if request.method in ["GET"]:
+            fields = [
+                "id",
+                "quizId",
+                "name",
+                "round_number",
+                "no_of_questions",
+                "no_of_schools",
+                "marks_per_question",
+                "marks_per_bonus_question",
+                "schoolParticipations",
+                "questions",
+            ]
+        else:
+            fields = self.Meta.fields
+
+        representation = super().to_representation(instance)
+        return {field: representation[field] for field in fields if field in representation}
